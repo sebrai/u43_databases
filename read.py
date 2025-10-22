529dad107ed59ae9e67ec76cdb6d1f614c3a2b1e
sebrai <sebbranden@gmail.com>
2025-10-22 11:37:42 +0200

    created read.py
    reads contents of choosen table

---
+++ read.py
@@ -0,0 +1,28 @@
+import mysql.connector
+import sys
+try:
+    with open("password.txt","r")  as f:
+        password = f.read()
+except FileNotFoundError:
+    print("password file doesnt exist")
+
+mydb = mysql.connector.connect(
+    host = "10.200.14.13",
+    port= 3306,
+    user = 'extsebrai',
+    password = password, 
+    database = 'musikk'
+)
+mycursor = mydb.cursor()
+# ^base info required for connection^
+
+# qeuries the user of the program
+read_choice= input("withc database should i show?: \033[32martists\033[0m , \033[32malbums\033[0m , \033[32msongs\033[0m: ")
+if not ["artists","albums","songs"]:
+    mycursor.execute("SELECT * FROM %s ",(read_choice,))
+else:
+    print(f"\033[31m \"{read_choice}\" \033[0m is not a valid option")
+    sys.exit()
+#print info
+for info in mycursor:
+    print(info)
